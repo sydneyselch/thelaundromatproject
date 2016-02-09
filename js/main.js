c17ac1b12390d65078c83288318be2eb6a9372c3
Sydney Selch <sydney_selch@Sydneys-MacBook-Pro.local>
2016-02-08 22:43:19 -0500

    feat(js): smooth scroll added but no links to connect to yet

--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
-var currentColour = 0;
-var colours = ['pink', 'teal', 'maroon', 'olive', 'purple', 'orange', 'navy', 'gray'];
-var $btn = $('.colour-btn');
+$('.desktop-nav, .mobile-nav').localScroll();
 
-$btn.on('click', function () {
-    $('.lb-nav li, .footer, .lb-close, border-btm').removeClass(colours[currentColour]);
-    
-     currentColour++;
-    
-    if (currentColour >= colours.length) {
-        currentColour = 0;    
-    }
-    
-    $('.lb-nav li, .footer, .lb-close, .border-btm').addClass(colours[currentColour]);
-});
-
-$('.desktop-nav, .nav').localScroll();
-
-
-
-
-var $links = $('.top-nav-li a');
-
-$links.on('click', function () {
-     $('.nav-check').prop('checked', false);
-});
 
 
    
