4514fb58f16e676fb560ec1012e8085aa1b389a2
Sydney Selch <sydney_selch@Sydneys-MacBook-Pro.local>
2016-03-03 20:12:18 -0500

    feat(nav): push nav added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
 $('.desktop-nav, .mobile-nav').localScroll();
 
+// Needs latest version of jQuery to run
+
+$(function(){
+    // shows buttons when js runs
+    $('body').removeClass('noscript');
+    
+    //sets buttons to toggle data state
+    $('.toggle-btn, .close-btn').click(function(){
+        toggleNav();
+      });
+
+  });
+
+function toggleNav(){
+    if ($('.site-wrapper').attr('data-state') == 'slide-closed') {
+        //display nav when closed
+        
+        $('.site-wrapper').attr('data-state','slide-open');
+        
+    } else {
+        
+        // hide nav when open 
+        $('.site-wrapper').attr('data-state','slide-closed');
+    }
+       
+}
+
 
 
    
